8897c709dca0938e9988800e4ec1ed5b7b94bf45
Shunsuke Shiino <shunsuke.shiino@gmail.com>
2012-01-27 19:28:27 +0900

    add new template.

---
+++ python/tests_template.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+
+import unittest
+
+class Tests(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_test(self):
+        self.assertEquals('','')
+
+
+if __name__ == '__main__':
+    unittest.main()
