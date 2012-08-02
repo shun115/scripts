96a0b0bdf3e4f4224de5238b4775d1cb8d348073
shiino-shunsuke <shiino.shunsuke@mountposition.co.jp>
2012-08-02 18:44:47 +0900

    first spec

--- ruby/rspec_training/message_filter_spec.rb
+++ ruby/rspec_training/message_filter_spec.rb
@@ -5,4 +5,10 @@ require 'rspec'
 require './message_filter'
 
 describe MessageFilter do
+
+  it 'should detect message with NG word' do
+    filter = MessageFilter.new 'foo'
+    filter.detect?('hello from foo').should be_true
+  end
+
 end
