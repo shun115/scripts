f527f20a9feb24858fdd1aa9641f4c8e0c032624
shiino-shunsuke <shiino.shunsuke@mountposition.co.jp>
2012-08-03 12:05:48 +0900

    Implement

--- ruby/rspec_training/message_filter.rb
+++ ruby/rspec_training/message_filter.rb
@@ -2,12 +2,15 @@
 
 class MessageFilter
 
-  def initialize(word)
-    @word = word
+  def initialize(*words)
+    @words = words
   end
 
   def detect?(text)
-    text.include? @word
+    @words.each do |w|
+      return true if text.include? w
+    end
+    false
   end
 
 end
