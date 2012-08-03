34d17e4aab21c9da7d36e8b1d131d89e4413274a
shiino-shunsuke <shiino.shunsuke@mountposition.co.jp>
2012-08-03 11:08:05 +0900

    Refactoring

--- ruby/rspec_training/message_filter_spec.rb
+++ ruby/rspec_training/message_filter_spec.rb
@@ -6,13 +6,15 @@ require './message_filter'
 
 describe MessageFilter do
 
+  before do
+    @filter = MessageFilter.new 'foo'
+  end
+
   it 'should detect message with NG word' do
-    filter = MessageFilter.new 'foo'
-    filter.detect?('hello from foo').should be_true
+    @filter.detect?('hello from foo').should be_true
   end
 
   it 'should not detect message with NG word' do
-    filter = MessageFilter.new 'foo'
-    filter.detect?('hello, world').should be_false
+    @filter.detect?('hello, world').should be_false
   end
 end
