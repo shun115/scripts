eaff31f0dc2deb8dae7076c72d808b1155406a39
shiino-shunsuke <shiino.shunsuke@mountposition.co.jp>
2012-08-03 12:12:47 +0900

    Refactoring

--- ruby/rspec_training/message_filter_spec.rb
+++ ruby/rspec_training/message_filter_spec.rb
@@ -4,10 +4,8 @@ require 'rubygems'
 require 'rspec'
 require './message_filter'
 
-describe MessageFilter, "with argument 'foo'" do
 
-  subject { MessageFilter.new 'foo' }
-
+share_examples_for "MessageFilter with argument 'foo'" do
 
   it { should be_detect('hello from foo') }
 
@@ -16,10 +14,20 @@ describe MessageFilter, "with argument 'foo'" do
 
 end
 
+describe MessageFilter, "with argument 'foo'" do
+
+  subject { MessageFilter.new 'foo' }
+
+  it_should_behave_like "MessageFilter with argument 'foo'"
+
+end
+
 describe MessageFilter, "with argument 'foo','bar'" do
 
   subject { MessageFilter.new('foo', 'bar') }
 
   it { should be_detect('hello from bar') }
+ 
+  it_should_behave_like "MessageFilter with argument 'foo'"
 
 end
